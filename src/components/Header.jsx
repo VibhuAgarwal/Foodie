5718e2de7390354ea3081b012dc1608aabdcbc5e
Vibhor Agarwal <agarwalvibhor930@gmail.com>
2024-04-09 20:35:32 +0530

    Created a reusable button component in any other component.

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,15 +1,16 @@
 import React from 'react';
-import logoimg from '../assets/logo.jpg'
+import logoImg from '../assets/logo.jpg'
+import Button from './UI/Button';
 
 export default function Header() {
     return (
         <header id='main-header'>
             <div id='title'>
-                <img src={logoimg} alt="A restaurant" id='img' />
+                <img src={logoImg} alt="A restaurant" id='img' />
                 <h1>FoodApp</h1>
             </div>
             <nav>
-                <button>Cart (0)</button>
+                <Button textOnly>Cart (0)</Button>
             </nav>
         </header>
     )
