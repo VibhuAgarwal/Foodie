099bca03288c3ab9d6c11e01165c200a96245a1d
Vibhor Agarwal <agarwalvibhor930@gmail.com>
2024-04-11 23:29:33 +0530

    Name changed

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -21,7 +21,7 @@ export default function Header() {
         <header id='main-header'>
             <div id='title'>
                 <img src={logoImg} alt="A restaurant" id='img' />
-                <h1>Foodie</h1>
+                <h1>Hungrier</h1>
             </div>
             <nav>
                 <Button textOnly onClick={handleShowCart}>Cart ({totalCartItems})</Button>
