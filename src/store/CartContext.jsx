954079286d5971eb75fcd865c471a6704910b148
Vibhor Agarwal <agarwalvibhor930@gmail.com>
2024-04-11 12:42:28 +0530

    Added checkout modal if cart is having any meal in it, Deploying to Github pages for the first time

--- src/store/CartContext.jsx
+++ src/store/CartContext.jsx
@@ -25,17 +25,16 @@ function cartReducer(state, action) {
     }
     if (action.type === 'REMOVE_ITEM') {
         const existingCartItemIdx = state.items.findIndex((item) => item.id === action.id);
-        const existingCartItem = state.items[existingCartItemIdx];
-
+        const existingItem = state.items[existingCartItemIdx];
         const updatedItems = [...state.items];
-        if (existingCartItem.quantity > 1) {
-            updatedItems.slice(existingCartItemIdx, 1)
-        } else {
-            const updatedItems = {
-                ...existingCartItem,
-                quantity: existingCartItem.quantity - 1,
+        if (existingItem.quantity > 1) {
+            const updatedItem = {
+                ...existingItem,
+                quantity: state.items[existingCartItemIdx].quantity - 1
             };
-            updatedItems[existingCartItemIdx] = updatedItems;
+            updatedItems[existingCartItemIdx] = updatedItem;
+        } else {
+            updatedItems.splice(existingCartItemIdx, 1);
         }
         return { ...state, items: updatedItems };
     }
@@ -45,11 +44,11 @@ function cartReducer(state, action) {
 export function CartContextProvider({ children }) {
     const [cart, dispatchCartAction] = useReducer(cartReducer, { items: [] });
     function addItem(item) {
-        dispatchCartAction({ type: 'ADD_ITEM', item: item })
+        dispatchCartAction({ type: 'ADD_ITEM', item })
     }
 
     function removeItem(id) {
-        dispatchCartAction({ type: 'REMOVE_ITEM', id: id })
+        dispatchCartAction({ type: 'REMOVE_ITEM', id })
     }
 
     const cartContext = {
